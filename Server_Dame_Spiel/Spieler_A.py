5e133908b2cc8b9247cd455ad4fbcd8273c97f0e
chichikolon <djimgoupat@yahoo.fr>
2018-02-20 17:46:44 +0100

    update

---
+++ Server_Dame_Spiel/Spieler_A.py
@@ -0,0 +1,22 @@
+# Python TCP Client A
+import socket
+
+host = socket.gethostname()
+port = 2008
+BUFFER_SIZE = 2000
+print("Host Name ist :  "+socket.gethostname())
+print("Host Adresse  : "+ socket.gethostbyaddr(socket.gethostname()))
+
+
+MESSAGE = raw_input("tcpClientA: Enter message/ Enter exit:")
+tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpClientA.connect((host, port))
+
+
+while MESSAGE != 'exit':
+    tcpClientA.send(MESSAGE)
+    data = tcpClientA.recv(BUFFER_SIZE)
+    print " Client2 received data:", data
+    MESSAGE = raw_input("tcpClientA: Enter message to continue/ Enter exit:")
+
+tcpClientA.close()
